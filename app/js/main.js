843ce09a0de80df54619b2c24baf3487e7ef3216
grishunion <grishunion@gmail.com>
2020-09-14 08:47:18 +0400

    adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -33,9 +33,17 @@ $(function(){
         $('.icon-th-list').removeClass('active');
 
     });
+    
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').slideToggle();
+    });
 
-    var mixer = mixitup('.products__inner-box');
+    $('.header__btn-menu').on('click', function(){
+        $('.header__box').toggleClass('active');
+    });
 
     
+
+    var mixer = mixitup('.products__inner-box');
 
 });
