6c43c759865421864ee83f45b1bf13dfd7f80e9d
grishunion <grishunion@gmail.com>
2020-10-01 06:09:26 +0400

    added withdrawals, upload

--- app/js/main.js
+++ app/js/main.js
@@ -44,12 +44,14 @@ $(function(){
 
     $('.product-one__tabs .tab, .settings__tabs .tab').on('click', function(event) {
         var id = $(this).attr('data-id');
-            $('.product-one__tabs, .settings__tabs').find('.tab-item').removeClass('active-tab').hide();
-            $('.product-one__tabs .tabs, .settings__tabs .tabs').find('.tab').removeClass('active');
-            $(this).addClass('active');
-            $('#'+id).addClass('active-tab').fadeIn();
-            return false;
-        });
+        $('.product-one__tabs, .settings__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.product-one__tabs .tabs, .settings__tabs .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#'+id).addClass('active-tab').fadeIn();
+        return false;
+    });
+
+    $('input[type="file"], select').styler();
 
     
 
